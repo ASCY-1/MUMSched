8dfee63fe8f9ab1ef9bcc22bb5ebe14256016e60
Cleave <cmasereka@gmail.com>
2017-04-04 22:26:33 -0500

    Testing Shell scripting

--- script.sh
+++ script.sh
@@ -1,9 +1,27 @@
-function b() {
+function gb(){
+   git branch
+}
+
+function gac(){
+   git add .
+   git commit -m"$1"
+   git push
+}
+
+function acp() {
     git add .
     git commit -m"$1"
     git push
 }
 
-function a(){
+function gs(){
    git status;
+}
+
+function cod(){
+   git checkout develop
+}
+
+function com(){
+   git checkout master
 }
